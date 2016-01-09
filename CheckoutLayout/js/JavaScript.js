f4ac0eae2e1615390f1e085cc39ea93a0cf1fd07
deepak.dubalmca@gmail.com <deepak.dubalmca@gmail.com>
2016-01-09 22:54:29 +0530

    ppp

--- CheckoutLayout/js/JavaScript.js
+++ CheckoutLayout/js/JavaScript.js
@@ -1,2 +1,25 @@
-﻿
+﻿function rescaleImage(b, f, c) {
+    var a = b.height;
+    var d = b.width;
+    var g = a / d;
+    if (a > f) {
+        g = f / a;
+        a = a * g;
+        d = d * g
+    }
+    if (d > c) {
+        g = c / d;
+        a = a * g;
+        d = d * g
+    }
+    b.width = d;
+    b.height = a;
+    if (d == 0 || a == 0) {
+        $(b).css('height', f);
+        $(b).css('width', c)
+    } else {
+        $(b).css('height', a);
+        $(b).css('width', d)
+    }
+}
 
